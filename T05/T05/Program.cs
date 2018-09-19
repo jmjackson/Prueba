6519683b751b134c4cdcd83b748cc22084015a44
LuisEstrada33 <43280434+LuisEstrada33@users.noreply.github.com>
2018-09-19 12:50:17 -0700

    Solucion de Luis

---
+++ T05/T05/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace T05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Stopwatch tiempo = Stopwatch.StartNew();
+
+           
+            
+            int suma=0;
+            for(int x =0; x<=1000000; x++)
+            {
+                suma = suma + x;
+
+            }
+            Console.WriteLine("La suma es: " + suma);
+            Console.Write("TIEMPO: " + tiempo.Elapsed.TotalMilliseconds.ToString() + " milisegundos");
+            Console.ReadKey();
+        }
+    }
+}
