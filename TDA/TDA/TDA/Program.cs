605498f092a3ac6012dafdcbe424287307ee1eff
Juan Hernandez <jarceluz@gmail.com>
2018-09-19 12:31:11 -0700

    Se incluyó la carpeta tda

---
+++ TDA/TDA/TDA/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TDALibrary;
+
+namespace TDA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circulo circle = new Circulo();
+            Console.WriteLine("Escribir el radio que del circulo");
+            circle.Radio = Convert.ToDecimal(Console.ReadLine());
+            circle.CalcularRadio(circle.Radio);
+        }
+    }
+}
---
+++ TDA/TDA/TDALibrary/Circulo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TDALibrary
+{
+    public class Circulo
+    {
+        public decimal Radio { get; set; }
+    
+        public Circulo()
+        {
+
+        }
+
+        public void CalcularRadio(decimal radio)
+        {
+            
+             Console.WriteLine("Este es el Radio "+radio);
+            Console.ReadKey();
+        }
+    }
+}
